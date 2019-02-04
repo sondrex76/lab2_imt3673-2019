03763b8510566338933996bed88fd5f78b1bd720
sondrex <sondrewr@stud.ntnu.no>
2019-02-04 13:52:54 +0100

    Activities added for lab 2

---
+++ app/src/main/java/com/example/sondre_lab_2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sondre_lab_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// Activity with list of items
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
