cdcb5a8a8e980482a6955e0c2fa6ab3ab1b4f0b1
sondrex <sondrewr@stud.ntnu.no>
2019-02-20 15:52:49 +0100

    Have added button to save settings and button to go to settings from main activty

--- app/src/main/java/com/example/sondre_lab_2/MainActivity.java
+++ app/src/main/java/com/example/sondre_lab_2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sondre_lab_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 // Activity with list of items
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button transfer = findViewById(R.id.btnOptions);
+        transfer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // Goes to the options menu
+                startActivity(new Intent(MainActivity.this, ActivityUserPreferences.class));
+            }
+        });
+
     }
 }
